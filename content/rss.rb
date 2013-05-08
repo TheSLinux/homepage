259d14448f93e807ead1b8617b0a6ea1bc733494
Ky-Anh Huynh <kyanh@theslinux.org>
2013-05-08 21:27:38 +0700

    rss: only gather itemes started by "news: " in subject

--- content/rss.rb
+++ content/rss.rb
@@ -5,6 +5,11 @@
 # Author : Anh K. Huynh
 # Date   : 2013 April 13th
 # License: GPL v2
+# Usage  :
+#   Every time a new commit is made on `contents/news/index.html`
+#   the feed will be updated. However, the commit subject must be
+#   started by `news: ` to be recorded by our script. This is to help
+#   we fix some typo error in the old pages.
 
 require "rss"
 require "git"
@@ -25,6 +30,7 @@ rss = RSS::Maker.make("2.0") do |maker|
     .object($object) \
     .since("40 weeks ago")\
     .each do |c|
+      next unless c.message.to_s.match(/^news: /)
       maker.items.new_item do |item|
         item.updated = c.date
         item.title = "#{c.message.split(/[\r\n]/).first}"
