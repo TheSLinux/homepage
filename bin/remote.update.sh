5b55a5cf2380b4bc5e2a64ca6d0e6bf862ca16ca
Ky-Anh Huynh <kyanh@theslinux.org>
2013-07-19 21:37:00 +0700

    bin.remote.update: support multiple nodes
    * tuxfamily {m4.theslinux.org, archlinuxvn.tuxfamily.org}
    * berlios (theslinux.berlios.de)

--- bin/remote.update.sh
+++ bin/remote.update.sh
@@ -1,38 +1,41 @@
 #!/bin/bash
 
-# Purpose: Update the page theslinux.berlios.d
+# Purpose: Update the page theslinux.berlios.de
 # Author : Anh K. Huynh
 # Date   : 2013 May 25th
+#          2013 Jul 19th
 # License: GPL v2
 
-_D_TMP="$HOME/tmp/"
-_D_DEST="/home/groups/theslinux/htdocs/"
-_F_SRC="theslinux.org.tgz"
-
 msg() {
-  echo "(berlios) $*"
+  echo "[$(hostname -f)] $*"
 }
 
 err() {
-  echo >&2 "(berlios) $*"
+  echo >&2 "[$(hostname -f)] $*"
 }
 
 die() {
-  err "$*"
+  err "[$(hostname -f)] $*"
   exit 1
 }
 
-_main() {
-  cd $_D_TMP || die "Can't switch to $_TMP_DIR"
+berlios_theslinux() {
+  local _D_DEST="/home/groups/theslinux/htdocs/"
+  local _F_SRC="theslinux.org.tgz"
+  local _D_TMP="$HOME/tmp/"
+
+  mkdir -p "$_D_TMP"
+  cd "$_D_DEST" || die "Can't switch to $_D_DEST"
+  cd "$_D_TMP" || die "Can't switch to $_D_TMP"
 
   msg "Downloading $_F_SRC from theslinux.org"
   rm -rf $_D_TMP/$_F_SRC $_D_TMP/theslinux.org/ \
   || die "Can't clean up the old file $_F_SRC"
 
   wget \
-      http://m0.theslinux.org/$_F_SRC?$(date +%s) \
+      "http://m0.theslinux.org/$_F_SRC?$(date +%s)" \
       -O $_F_SRC -o /dev/null \
-  || die "Can't download source file from theslinux.org"
+  || die "Can't download source file from m0.theslinux.org"
 
   msg "Extracting $_F_SRC"
   tar -xzf $_F_SRC
@@ -50,4 +53,77 @@ _main() {
   msg "Please report to the team if you see any error before this line"
 }
 
-_main
+#
+# Origin: https://github.com/archlinuxvn/home/blob/ \
+#     14d2a580c77811f6855b0408188516030967b0e2/bin/tux-update
+#
+tuxfamily_archlinuxvn() {
+  local _D_TMP="$HOME/archlinuxvn/archlinuxvn.tuxfamily.org-web/tmp/"
+  local _D_DEST="$HOME/archlinuxvn/archlinuxvn.tuxfamily.org-web/htdocs/"
+  local _F_SRC="archlinuxvn.org.tgz"
+
+  cd "$_D_DEST" || die "Can't switch to $_D_DEST"
+  cd "$_D_TMP" || die "Can't switch to $_D_TMP"
+
+  msg "Downloading $_F_SRC from archlinuxvn.org"
+  rm -rf $_D_TMP/$_F_SRC $_D_TMP/archlinuxvn.org/ \
+    || die "Can't clean up the old file $_F_SRC"
+  wget \
+      "http://m0.archlinuxvn.org/$_F_SRC?$(date +%s)" \
+      -O $_F_SRC -o /dev/null \
+    || die "Can't download source file from m0.archlinuxvn.org"
+
+  msg "Extracting $_F_SRC"
+  tar -xzf $_F_SRC
+
+  msg "Synchronizing files"
+  rsync \
+      -rap --delete "$_D_TMP/archlinuxvn.org/" \
+      --exclude="irclog*" $_D_DEST/ \
+    || die "Can't synchronize files from the variant directory to $_D_DEST"
+
+  msg "Fixing file permission"
+  chmod 700 $_D_DEST/irclogs/
+}
+
+tuxfamily_theslinux() {
+  local _D_TMP="$HOME/theslinux/m4.theslinux.org-web/tmp/"
+  local _D_DEST="$HOME/theslinux/m4.theslinux.org-web/htdocs/"
+  local _F_SRC="theslinux.org.tgz"
+
+  cd "$_D_DEST" || die "Can't switch to $_D_DEST"
+  cd "$_D_TMP" || die "Can't switch to $_D_TMP"
+
+  msg "Downloading $_F_SRC from theslinux.org"
+  rm -rf "$_D_TMP/$_F_SRC" "$_D_TMP/theslinux.org/" \
+    || die "Can't clean up the old file $_F_SRC"
+  wget \
+      "http://m0.theslinux.org/$_F_SRC?$(date +%s)" \
+      -O $_F_SRC -o /dev/null \
+    || die "Can't download source file from m0.theslinux.org"
+
+  msg "Extracting $_F_SRC"
+  tar -xzf $_F_SRC
+
+  msg "Synchronizing files"
+  rsync \
+      -rap --delete "$_D_TMP/theslinux.org/" \
+      $_D_DEST/ \
+    || die "Can't synchronize files from the variant directory to $_D_DEST"
+}
+
+_update() {
+  local _cmd="$SSH_ORIGINAL_COMMAND"
+  case "$_cmd" in
+    "berlios_theslinux")      ;;
+    "tuxfamily_archlinuxvn")  ;;
+    "tuxfamily_theslinux")    ;;
+    *) die "Unknown command '$_cmd'" ;;
+  esac
+
+  "$_cmd"
+}
+
+_update
+
+: Nothing lasts forever
