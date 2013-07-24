9439d3b791c9af234ad9e649ba7a2327f8899d6c
Ky-Anh Huynh <kyanh@theslinux.org>
2013-07-24 22:46:41 +0700

    git.hook.post-receive-email.sh: - $describe from commit subject
    For some reason, the tag do not reflect exactly what is going on. E.g,
    the :home repository has no tag, and tag is only useful for minor issue.
    While :system repository has many kind of tags.
    
    TOD: We should take a look at the use of $describe later

--- bin/git.hook.post-receive-email.sh
+++ bin/git.hook.post-receive-email.sh
@@ -232,7 +232,7 @@ generate_email_header()
 	# Generate header
 	cat <<-EOF
 	To: $recipients
-	Subject: ${emailprefix}$projectdesc $refname_type $short_refname ${change_type}d. $describe
+	Subject: ${emailprefix}$projectdesc $refname_type $short_refname ${change_type}d.
 	MIME-Version: 1.0
 	Content-Type: text/plain; charset=utf-8
 	Content-Transfer-Encoding: 8bit
