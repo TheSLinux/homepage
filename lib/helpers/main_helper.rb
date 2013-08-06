ca5d2c62d2ce10d8defb5ea34f20a773367e1ec2
Ky-Anh Huynh <kyanh@theslinux.org>
2013-08-06 20:31:04 +0700

    main-helper: clean license text

--- lib/helpers/main_helper.rb
+++ lib/helpers/main_helper.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 ########################################################################
 # WARNING: normal authors are not allowed to change this file.         #
 #          all changes will be simply ignored by server                #
@@ -195,10 +196,10 @@ EOF
          "Bạn được <b>Sao chép</b>, <b>Chia sẻ</b>, <b>Phân phối</b> trang này dưới điều kiện sau:",
          "",
          "(1) Bạn phải ghi tên tác giả <b>TheSLinux</b> và giấy phép; tuy nhiên <b>không</b>",
-         "    được hàm ý tác giả  trao trang này hay quyền sử dụng trang này cho bạn;",
-         "(2) Nếu bạn sử dụng, chuyển đổi, hoặc xây dựng dự án từ nội dung được chia sẻ này,",
-         "    bạn phải áp dụng giấy phép này hoặc giấy phép khác có các điều khoản tương tự",
-         "    như giấy phép này cho dự án của bạn."
+         "    được hàm ý tác giả trao trang này hay quyền sử dụng trang này cho bạn;",
+         "(2) Nếu bạn sử dụng, chuyển đổi, hoặc xây dựng dự án từ trang này,",
+         "    bạn phải áp dụng giấy phép BY-SA hoặc giấy phép có các điều khoản"
+         "    tương tự như giấy phép By-SA cho dự án của bạn."
         ]
       when false
         []
